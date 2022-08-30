353a79b5c44eff38c0d560f779873fb2433374f1
ulmans <abramovxz@gmail.com>
2022-08-31 02:25:17 +0300

    Add SVG-icons

---
+++ src/assets/icons/CloseIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18 6L6 18" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" class="icon"/>
+        <path d="M6 6L18 18" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" class="icon"/>
+    </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+</style>
